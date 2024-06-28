5074ce7b9be00e7ff9fe8199b69572a99b53ebb2
vanderscycle <henri-vandersleyen@protonmail.com>
2024-06-27 20:59:53 -0700

    reset

--- flake.nix
+++ flake.nix
@@ -7,22 +7,17 @@
   outputs =
     { self, nixpkgs, ... }@inputs:
     let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix;
-
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-      });
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = system;
+        config.allowUnfree = true;
+      };
     in
     {
-
-# https://discourse.nixos.org/t/basic-flake-run-existing-python-bash-script/19886/2
+      # https://discourse.nixos.org/t/basic-flake-run-existing-python-bash-script/19886/2
       # nix run ".#output1"
-      output1 = {config, ...}:
-      with nixpkgs.lig;
-      {
-      pkgs.writeScriptBin.myscript = ''
+      output1 = pkgs.writeScriptBin "myscript" ''
         echo foo
       '';
-      };
     };
 }
