83a865d10a410279caa7bad32692e5d4ab8672ab
vanderscycle <henri-vandersleyen@protonmail.com>
2024-06-27 21:03:05 -0700

    reset 2

--- flake.nix
+++ flake.nix
@@ -5,19 +5,26 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
-    { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        system = system;
-        config.allowUnfree = true;
-      };
-    in
     {
-      # https://discourse.nixos.org/t/basic-flake-run-existing-python-bash-script/19886/2
-      # nix run ".#output1"
-      output1 = pkgs.writeScriptBin "myscript" ''
-        echo foo
-      '';
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        packages = {
+          myscript = pkgs.writeScriptBin "myscript" ''
+            echo foo
+          '';
+        };
+      }
+    );
 }
