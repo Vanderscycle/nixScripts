298baa3a2e033549a7561dc4e871a63ede87be31
vanderscycle <henri-vandersleyen@protonmail.com>
2024-06-27 20:26:52 -0700

    test

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Testing writers for nix";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+
+      # https://discourse.nixos.org/t/basic-flake-run-existing-python-bash-script/19886/2
+      # nix run ".#output1"
+      output1 = pkgs.writeScriptBin "myscript" ''
+        echo foo
+      '';
+    };
+}
