59b3fefca4e9f9533dd2fd09d3b68947884ed0ab
vanderscycle <henri-vandersleyen@protonmail.com>
2024-06-28 21:57:43 -0700

    update

--- flake.nix
+++ flake.nix
@@ -24,6 +24,17 @@
           myscript = pkgs.writeScriptBin "myscript" ''
             echo foo
           '';
+          output2 = pkgs.writeScriptBin "myscript" ''
+            chmod 777 myscript.sh
+            ./myscript.sh;
+          '';
+          # nix run/build '.#output3'
+          # if nix build was :xa
+          output3 = pkgs.writeScriptBin "myscript" ''
+            export PATH=${pkgs.lib.makeBinPath [ pkgs.hello ]}:$PATH
+            chmod 777 run-hello.sh
+            ${./run-hello.sh}
+          '';
         };
       }
     );
